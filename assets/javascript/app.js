978fb5c58ba07a6988c99d35d0e16fe426a95982
b0bland <alfredychan96@gmail.com>
2019-06-10 20:25:35 -0700

    Added firebase SDK snippet

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCV4PCOZM7I3lTo-trs-Ur5yfbOpddbjGw",
+    authDomain: "funky-f48ad.firebaseapp.com",
+    databaseURL: "https://funky-f48ad.firebaseio.com",
+    projectId: "funky-f48ad",
+    storageBucket: "funky-f48ad.appspot.com",
+    messagingSenderId: "915541038910",
+    appId: "1:915541038910:web:105d16f36335da85"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
